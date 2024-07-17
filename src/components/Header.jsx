aaf00f1762332addba92fc789f0f71a902053e89
Imre <decodeimre@gmail.com>
2024-07-17 14:14:46 +0200

    implemented bootstrap dropdown component for menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,47 @@
+import Dropdown from "react-bootstrap/Dropdown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// Import the FontAwesome library
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+// Import the specific icons you need
+import { faBars, faUser, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { DropdownMenu } from "react-bootstrap";
+
+// Add the imported icons to the library
+library.add(faBars, faUser, faPlus);
+
+function MenuDropDownLeft() {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle as="button" id="dropdown-menu">
+        <FontAwesomeIcon icon="bars" />
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+function MenuDropDownRight() {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle as="button" id="dropdown-menu">
+        <FontAwesomeIcon icon="user" />
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
 export function Header() {
   return (
     <div className="container-fluid bg-dark text-white p-3">
@@ -9,13 +53,13 @@ export function Header() {
       {/*navigation with button burger-menu, welcome text, button user-menu*/}
       <div className="row">
         <div className="col-2 text-center">
-          <button className="menu-button">☰</button>
+          <MenuDropDownLeft />
         </div>
         <div className="col-8 text-center">
           <h2>Welcome, User!</h2>
         </div>
         <div className="col-2 text-center">
-          <button className="user-button">👤</button>
+          <MenuDropDownRight/>
         </div>
       </div>
     </div>
