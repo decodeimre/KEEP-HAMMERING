26f84d292c0013e9bdd5ff3711f54302714ee8f8
Imre <decodeimre@gmail.com>
2024-08-21 16:01:57 +0200

    calendar options checked.events and eventcontent not yet connected

--- frontend/src/components/utils/Calendar.jsx
+++ frontend/src/components/utils/Calendar.jsx
@@ -2,6 +2,7 @@ import FullCalendar from "@fullcalendar/react";
 import interactionPlugin from "@fullcalendar/interaction";
 import multiMonthPlugin from "@fullcalendar/multimonth";
 import { DateContext } from "../context/dateContext";
+import { ExerciseLogsContext } from "../context/exerciseLogsContext";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -16,7 +17,19 @@ library.add(faCircleDot, faAngleLeft);
 
 export default function MyCalendar() {
 
-  const {date, setCurrentDate} = useContext(DateContext)
+  const muscleColors = {
+    Abs: "red",
+    Back: "blue",
+    Biceps: "yellow",
+    Chest: "green",
+    Legs: "orange",
+    Shoulders: "purple",
+    Triceps: "grey"
+};
+
+  const {date, setCurrentDate} = useContext(DateContext);
+  const {exerciseLogs} = useContext(ExerciseLogsContext);
+
   const navigate = useNavigate();
     //example handleClick function
     //should later open day view with planned workout and option to add new workout
@@ -29,6 +42,7 @@ export default function MyCalendar() {
       <div className="container calendar-container">
       <div id="calendar">
         <FullCalendar
+          style={{cursor: "pointer"}}
           plugins={[multiMonthPlugin, interactionPlugin]}
           initialView="multiMonthYear"
           multiMonthMaxColumns={1}
@@ -40,17 +54,24 @@ export default function MyCalendar() {
               title: "awesome" , // a property!
               start: "2024-09-01", // a property!
               end: "2024-09-02", // see important note below about 'end'
-              color: "pink", // a property!
+              color: "whitesmoke" // a property!
+            },
+            {
+              title: "awesome" , // a property!
+              start: "2024-09-03", // a property!
+              end: "2024-09-04", // see important note below about 'end'
+              color: "whitesmoke" // a property!
             },
             
           ]}
-          //this is how we can display icons. (could be callback handling all targetmuscles from exerciselist and create icon array)
+          //this is how we can display icons. (could be callback handling all targetmuscles from exerciseLoglist and create icon array)
+          //now the eventContent works for all events - how to connect the right one?!?!
           eventContent={[<FontAwesomeIcon
               icon="fa-solid fa-circle-dot"
-              style={{ color: "blue", marginRight: "2rem" }}
+              style={{ color: "blue", marginRight: ".5rem" }}
             />, <FontAwesomeIcon
             icon="fa-solid fa-circle-dot"
-            style={{ color: "red", marginRight: "2rem" }}
+            style={{ color: "red", marginRight: ".5rem" }}
           />]}
         />
       </div></div>
